784fe9a9e39f7deeabc66bbe88e42c1fec535cd2
yehezkielwinatali <yehezkielangg@gmail.com>
2025-06-01 01:19:31 +0900

    remove greeting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,11 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
-import GreetingAlert from "../components/GreetingAlert";
 import { useState } from "react";
 
 const Home = () => {
-  const [showGreeting, setShowGreeting] = useState(true);
-  const closeGreeting = () => {
-    setShowGreeting(false);
-  };
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {showGreeting && <GreetingAlert onClose={closeGreeting} />}
       {/* Theme Toggle */}
       <ThemeToggle />
 
